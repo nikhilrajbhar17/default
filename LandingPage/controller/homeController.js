c41e9c643b62435df0890d821ead7a4536e81bfa
Cold-fish78 <pubg69086@gmail.com>
2023-12-14 22:49:54 +0530

    added calc

--- LandingPage/controller/homeController.js
+++
@@ -1,3 +0,0 @@
-module.exports.homeView = function (req, res) {
-   return res.sendFile("/landingPage.html", { root: __dirname });
-};
--- LandingPage/controller/pageController.js
+++
@@ -1,3 +0,0 @@
-module.exports.baliView = function (req, res) {
-  res.render("bali", { title: "bali", layout: "../views/loyout/full-width" });
-};
